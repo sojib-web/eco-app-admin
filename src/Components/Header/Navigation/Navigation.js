ab8c14e271bc8767a916773493436124268e3f76
unknown <alisojib295@gmail.com>
2024-04-21 16:04:55 +0600

    6 times video 6

--- src/Components/Header/Navigation/Navigation.js
+++ src/Components/Header/Navigation/Navigation.js
@@ -3,13 +3,16 @@ import { Button } from "@mui/material";
 import { IoMdMenu } from "react-icons/io";
 import { GoTriangleDown } from "react-icons/go";
 import { Link } from "react-router-dom";
-
+import { CiHome } from "react-icons/ci";
+import { CiShop } from "react-icons/ci";
+import { DiAtom } from "react-icons/di";
+import { AiOutlineCoffee } from "react-icons/ai";
 const Navigation = () => {
   return (
     <nav>
       <div className="container">
         <div className="row">
-          <div className="col-sm-3 navpart1">
+          <div className="col-sm-2 navpart1">
             <Button className="allatTab align-items-center">
               <span className="icon1 mr-2">
                 {" "}
@@ -23,31 +26,55 @@ const Navigation = () => {
             </Button>
           </div>
 
-          <div className="col-sm-9 navpart2">
-            <ul className="list list-inline">
+          <div className="col-sm-10 navpart2 d-flex  align-items-center">
+            <ul className="list list-inline ml-auto">
               <li className="list-inline-item">
-                <Link to="/">Home</Link>
+                <Link to="/">
+                  {" "}
+                  <CiHome /> &nbsp; Home
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/">Shop</Link>
+                <Link to="/">
+                  {" "}
+                  <CiShop /> &nbsp; Shop
+                </Link>
               </li>{" "}
               <li className="list-inline-item">
-                <Link to="/"> Bakery</Link>
+                <Link to="/">
+                  {" "}
+                  <DiAtom /> &nbsp; Bakery
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/"> Breakfast & Dairy</Link>
+                <Link to="/">
+                  {" "}
+                  <AiOutlineCoffee /> &nbsp; Coffee
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/">Coffee</Link>
+                <Link to="/">
+                  {" "}
+                  <AiOutlineCoffee /> &nbsp;Fishion
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/"> Biscuits & Snacks</Link>
+                <Link to="/">
+                  {" "}
+                  <AiOutlineCoffee /> &nbsp; Elecrtonic
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/"> Grocery & Staples</Link>
+                <Link to="/">
+                  {" "}
+                  <AiOutlineCoffee /> &nbsp; Blog
+                </Link>
               </li>
               <li className="list-inline-item">
-                <Link to="/">Contact Us</Link>
+                <Link to="/">
+                  {" "}
+                  <AiOutlineCoffee /> &nbsp;Contact Us
+                </Link>
               </li>
             </ul>
           </div>
