a3bce208073e92149d5046232fa6e9e494800101
unknown <alisojib295@gmail.com>
2024-04-23 17:57:01 +0600

    9 done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,21 +1,23 @@
 import HomeBanner from "../../Components/HomeBanner/HomeBanner";
-import BannerImg from "../../assets/image/banner-box.jpg"
+import BannerImg from "../../assets/image/banner-box.jpg";
 import { Button } from "@mui/material";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import React from "react";
-import Slider from "react-slick";
-const Home = () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import productImg from "../../assets/image/product1.jpg";
+import Rating from "@mui/material/Rating";
 
-  var productsSliderOption = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1
-  };
+import { SlSizeFullscreen } from "react-icons/sl";
+import ProductsItem from "../../Components/ProductsItem/ProductsItem";
+
+const Home = () => {
   return (
     <>
       <HomeBanner />
+      <br /> <br />
       <section className="homeProducts">
         <div className="container">
           <div className="row">
@@ -28,18 +30,50 @@ const Home = () => {
               <div className="d-flex align-items-center">
                 <div className="info ">
                   <h3 className="mb-0 hd">Best Sellers</h3>
-                  <p className="mb-0">Do not miss the current offers until the end of March.</p>
+                  <p className="mb-0">
+                    Do not miss the current offers until the end of March.
+                  </p>
                 </div>
 
-                <Button className="viewAllBtn ml-auto">View All &nbsp; <FaArrowAltCircleRight /></Button>
+                <Button className="viewAllBtn ml-auto">
+                  View All &nbsp; <FaArrowAltCircleRight />
+                </Button>
               </div>
 
-              <div className="product_row">
-                <Slider {...productsSliderOption}>
-                  <div className="item productItem">
+              <div className="product_row w-100 mt-4">
+                <Swiper
+                  slidesPerView={4}
+                  spaceBetween={0}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={[Navigation]}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
 
-                  </div>
-                </Slider>
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <ProductsItem />
+                  </SwiperSlide>
+                </Swiper>
               </div>
             </div>
           </div>
